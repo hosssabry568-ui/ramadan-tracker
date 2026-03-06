8bd19ca75d336a5b5c066ba572fe8f3cead8e543
hosssabry568-ui <hosssabry568@gmail.com>
2026-03-06 14:36:09 +0200

    Delete app/[locale]/page.tsx

--- app/[locale]/page.tsx
+++
@@ -1,9 +0,0 @@
-import RamadanTracker from '@/components/layout/ramadan-tracker';
-
-export default function Home() {
-  return (
-    <main className='min-h-screen bg-gradient-to-b from-purple-50 to-white'>
-      <RamadanTracker />
-    </main>
-  );
-}
