8b56722f210926c4ca47a800cfa6409662642135
hosssabry568-ui <hosssabry568@gmail.com>
2026-03-06 16:28:49 +0200

    Change default theme to 'system' in layout

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -161,7 +161,7 @@ export default async function RootLayout({
         <NextIntlClientProvider messages={messages}>
           <ThemeProvider
             attribute='class'
-            defaultTheme='light'
+            defaultTheme='system'
             enableSystem
             disableTransitionOnChange
           >
