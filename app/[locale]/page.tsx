ec0e13168b53f49dbed7ca68f78f10a257a623b8
hosssabry568-ui <hosssabry568@gmail.com>
2026-03-06 14:36:21 +0200

    Add files via upload

---
+++ app/[locale]/page.tsx
@@ -0,0 +1,9 @@
+import RamadanTracker from '@/components/layout/ramadan-tracker';
+
+export default function Home() {
+  return (
+    <main className='min-h-screen bg-gradient-to-b from-purple-50 to-white dark:from-gray-950 dark:to-gray-900'>
+      <RamadanTracker />
+    </main>
+  );
+}
